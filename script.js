d1d3fb200d088ac66bd1d6b9b87f258f65061242
Daniel <danielsousasilva1811@gmail.com>
2025-12-19 14:47:16 -0300

    Criadas as paginas de teste

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = prompt("Digite o seu nome:")
+let idade = prompt("Digite a sua odade:")
+
+if (idade >= 18) {
+    console.log("Vc é maior de idade!")
+} else {
+    console.log("Voc é menor de idade!")
+}
